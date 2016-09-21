910d76da4954b8aed06456c0f21f3c86061e3168
Andrew Harris <blakebharris@gmail.com>
2016-09-20 22:38:23 -0500

    started adding relationships

--- tbay.py
+++ tbay.py
@@ -10,31 +10,37 @@ Base = declarative_base()
 
 
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Item(Base):
-    __tablename__ = 'items'
+    __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
 
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    items = relationship("Item", backref="User")
+    bids = relationship("Bid", backref="User")
 
 
 class Bid(Base):
-    __tablename__ = 'bids'
+    __tablename__ = 'bid'
 
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
+
 
 
 Base.metadata.create_all(engine)
