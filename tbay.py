20a6b7de0e1d88ea411eea475cb2a53f7776c96a
Andrew Harris <blakebharris@gmail.com>
2016-09-21 21:18:34 -0500

    added relationship

--- tbay.py
+++ tbay.py
@@ -8,39 +8,70 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Item(Base):
-    __tablename__ = 'item'
+    __tablename__ = 'items'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
 
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    items = relationship("Item", backref="User")
-    bids = relationship("Bid", backref="User")
 
 
 class Bid(Base):
-    __tablename__ = 'bid'
+    __tablename__ = 'bids'
 
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
 
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    item = relationship('Item', backref='bids')
+
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship('User', backref='bids')
+
 
 
 Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    from tbay import session
+    from sqlalchemy import desc
+
+    beyonce = User(username='bknowles', password='123456')
+    christina = User(username='caguillera', password='qweasd')
+    britney = User(username='bspears', password='secret')
+
+    microphone = Item(name="microphone",
+                      description="Mika's gold microphone.")
+
+    bids = [
+         Bid(price=500.0, user=christina, item=microphone),
+         Bid(price=150.0, user=beyonce, item=microphone),
+         Bid(price=200.0, user=christina, item=microphone),
+         Bid(price=201.0, user=beyonce, item=microphone),
+    ]
+
+    session.add_all([beyonce, christina, britney, microphone] + bids)
+    session.commit()
+
+    highest_bid = (session.query(Bid)
+                   .filter(Bid.item==microphone)
+                   .order_by(desc(Bid.price))
+                   .first())
+
+    print(highest_bid.user.username)
+    print(highest_bid.item.description)
